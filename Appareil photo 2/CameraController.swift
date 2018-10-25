b62b1dddb379e2c6824fc87d2221dd14a4bbcd2a
Thierry Huu <huuth@macbook-pro-de-thierry.local>
2018-10-25 15:45:32 +0200

    Mise en place du projet

---
+++ Appareil photo 2/CameraController.swift
@@ -0,0 +1,29 @@
+//
+//  CameraController.swift
+//  Appareil photo 2
+//
+//  Created by Thierry Huu on 25/10/2018.
+//  Copyright © 2018 Thierry Huu. All rights reserved.
+//
+
+import UIKit
+
+class CameraController: UIViewController {
+
+    @IBOutlet weak var cameraView: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    @IBAction func prendrePhoto(_ sender: Any) {
+    }
+    
+    @IBAction func versLibrarie(_ sender: Any) {
+    }
+    
+    @IBAction func rotationCamera(_ sender: Any) {
+    }
+}
+
